761fc54149bc31f1dd23733dbaca363d6c73cd11
cris <cristsilva4676@gmail.com>
2021-12-07 20:59:46 -0300

    adicionando projeto pricipal

---
+++ ContaCorrente.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaixaelEtronicoUp
+{
+    public class ContaCorrente
+    {
+        public Cliente titular;
+        public int numeroAgencia;
+        public int numero = 01;
+        public double _saldo;
+        public double valor;
+        
+        public double Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+
+            }
+
+        }
+       
+        public bool sacar(double valor)
+        {
+            if (this._saldo < valor)
+            {
+                Console.WriteLine("saldo insuficiente");
+
+
+            }
+            if (this._saldo < valor)
+            {
+                return false;
+
+            }
+            else
+            {
+                this._saldo -= valor;
+                return true;
+
+            }
+
+
+        }
+        public void depositar(double valor)
+        {
+            Console.WriteLine(" voce depositou " + valor);
+
+            this._saldo += valor;
+
+            
+        }
+        public bool transferir(double valor, ContaCorrente contadestino)
+        {
+            if (this._saldo < valor)
+            {
+                return false;
+
+            }
+            else
+            {
+                this._saldo -= valor;
+                contadestino.depositar(valor);
+                return true;
+
+            }
+
+
+        }
+
+
+
+    }
+    
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace CaixaelEtronicoUp
+{
+    class Program
+    {
+        public static ConsoleKeyInfo Keyinfo;
+        static void Main(string[] args)
+        {
+            
+
+            ContaCorrente [] conta = new ContaCorrente[5];
+
+            conta[0].titular = new Cliente();
+
+             
+            
+           
+           
+          if (conta == null)
+          {
+             for (int posição = 0; posição < conta.Length; posição++)
+             {
+                 Console.WriteLine("login :");
+                 conta[1].titular.nome = Console.ReadLine();
+                 Console.Write("senha :");
+                 conta[1].titular.senha = Console.ReadLine();
+                 Console.WriteLine(" F1 para criar conta ");
+             }
+                    
+                    Keyinfo = Console.ReadKey();
+           }
+            if (Keyinfo.Key == ConsoleKey.F1)
+            {
+               Console.WriteLine("digite seu nome: ");
+               conta[0].titular.nome = Console.ReadLine();
+               Console.WriteLine("digite sua senha: ");
+               conta[0].titular.senha = Console.ReadLine();
+               Console.WriteLine("digite sua CPF");
+               conta[0].titular.cpf = Console.ReadLine();
+
+            }
+                
+            
+            
+           
+
+        }
+        
+    }
+}
